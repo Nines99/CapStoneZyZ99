bc2ed7aa15044948672528f130a9d0dea67292ec
Nines99 <kirbyhenry99@gmail.com>
2024-01-11 10:55:05 +1300

    update frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ResponsiveAppBar from './components/NavBar'
+// import ResponsiveAppBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <ResponsiveAppBar/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,6 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
